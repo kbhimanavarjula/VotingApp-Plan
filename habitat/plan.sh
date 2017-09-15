a69f08441c90ef83366f9b9984bd3339f13af411
root <root@ossnode2.f1vrntz235aedas3md3gyjdz0b.dx.internal.cloudapp.net>
2017-09-15 11:05:54 +0000

    updated hart file

---
+++ habitat/plan.sh
@@ -0,0 +1,101 @@
+pkg_name=VotingApp
+pkg_origin=kbhimanavarjula
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('GPU')
+pkg_source="https://github.com/kbhimanavarjula/VotingApp"
+pkg_build_deps=(core/git core/virtualenv)
+pkg_deps=(core/coreutils core/python2)
+#pkg_exports=([http]=8080)
+pkg_expose=(8080)
+pkg_svc_user="root"
+
+do_download()  
+{
+        build_line "do_download() =================================================="  
+        cd ${HAB_CACHE_SRC_PATH}
+
+        build_line "\$pkg_dirname=${pkg_dirname}"  
+        build_line "\$pkg_filename=${pkg_filename}"
+
+        if [ -d "${pkg_dirname}" ];  
+        then  
+            rm -rf ${pkg_dirname}  
+        fi
+
+        mkdir ${pkg_dirname}  
+        cd ${pkg_dirname}  
+        GIT_SSL_NO_VERIFY=true git clone --branch master https://github.com/kbhimanavarjula/VotingApp.git
+        return 0  
+}
+
+do_verify() {
+	
+	return 0
+}
+do_clean() {
+	build_line "do_clean() ===================================================="
+	return 0
+}
+
+do_unpack() {
+	build_line "do_unpack() ===================================================="
+	return 0
+}
+
+do_build() {
+	build_line "do_build() ===================================================="
+	return 0
+}
+
+do_install() {
+	build_line "do_install() ===================================================="
+	#cd $pkg_prefix
+	virtualenv venv
+    	source venv/bin/activate
+	local source_dir="${HAB_CACHE_SRC_PATH}/${pkg_dirname}/${pkg_filename}"
+        build_line "source dir is ${source_dir}"
+        pip install -r "${source_dir}/vote/requirements.txt"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
+# pkg_shasum="TODO"
+# pkg_deps=(core/glibc)
+# pkg_build_deps=(core/make core/gcc)
+# pkg_lib_dirs=(lib)
+# pkg_include_dirs=(include)
+# pkg_bin_dirs=(bin)
+# pkg_pconfig_dirs=(lib/pconfig)
+# pkg_svc_run="bin/haproxy -f $pkg_svc_config_path/haproxy.conf"
+# pkg_exports=(
+#   [host]=srv.address
+#   [port]=srv.port
+#   [ssl-port]=srv.ssl.port
+# )
+# pkg_exposes=(port ssl-port)
+# pkg_binds=(
+#   [database]="port host"
+# )
+# pkg_binds_optional=(
+#   [storage]="port host"
+# )
+# pkg_interpreters=(bin/bash)
+# pkg_svc_user="hab"
+# pkg_svc_group="$pkg_svc_user"
+# pkg_description="Some description."
+# pkg_upstream_url="http://example.com/project-name"
